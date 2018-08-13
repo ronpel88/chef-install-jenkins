3f81283b3655e9a30c98acc8061770c3e042173b
Ron Shaiko <ronshaiko@Rons-MacBook-Pro.local>
2018-08-13 11:55:40 +0300

    added more resources to recipe

--- cookbooks/jenkins/recipes/default.rb
+++ cookbooks/jenkins/recipes/default.rb
@@ -1,13 +1,46 @@
-# recipe that installs jenkins
+# recipe that installs jenkins on ubuntu machine
+
+# log a welcome message
+log 'welcome_message' do
+  message 'Hello Ron, the Creator of this amazing recipe!'
+  level :info
+end
+
+# log another welcome message
+log 'welcome_message2' do
+  message 'going to install prerequisites & jenkins'
+  level :info
+end
+  
+# helper packages for development purposes
+# nmap - gives info about network on a system
+['nmap','wget','git', 'python-pip', 'groovy', 'ruby-full'].each do |p|
+  package p do
+    action :install
+  end
+end
 
 # install java
 package "default-jre"
 
-# add java location to path
+# get java path and save to variable
+ruby_block "get_java_location" do
+  block do
+    s = shell_out("which java")
+    node.default['java_location'] = s.stdout
+  end
+end
+
+# add java path to general path
 ruby_block 'set_java_path' do
   block do
-    ENV['PATH'] = "#{ENV['PATH']}:/usr/bin/java"
+    ENV['PATH'] = "#{ENV['PATH']}:#{node['java_location']}"
   end
+end
+
+
+execute 'set_JAVA_HOME' do
+  command 'echo JAVA_HOME="`which java`" | tee -a /etc/environment'
 end
 
 # log path
